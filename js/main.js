38491812fde15259babd2ff9fe6e9c9cdc7e90fb
iladushek <iladiy21@gmail.com>
2018-02-25 16:38:43 +0200

    first cpmmit

---
+++ js/main.js
@@ -0,0 +1,16 @@
+     $(document).ready(function(){
+
+
+      $('.slider').slick({
+         infinite: true,
+  		 slidesToShow: 1,
+ 		 slidesToScroll: 1,
+ 		 arrows: false,
+ 		 dots: true,
+ 		 dotsClass: "my-dots"
+    });
+
+
+
+
+    });
